87eb52247501a653f49077c9bf8a97567bf0a535
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-12-15 23:55:41 +0900

    test applicationContext.xml

---
+++ Spring02_bean/src/com/test02/Address.java
@@ -0,0 +1,25 @@
+package com.test02;
+
+public class Address {
+	private String name;
+	private String addr;
+	private String phone;
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Address(String name, String addr, String phone) {
+		super();
+		this.name = name;
+		this.addr = addr;
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "name=" + name + "\t addr=" + addr + "\t phone=" + phone;
+	}
+		
+}
