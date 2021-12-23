5034cdcdf5d57ed1fc18f1490e5bf5d3f97750d1
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-12-23 23:56:00 +0900

    constructor로 bean생성자 값넣기

---
+++ Spring03/src/com/test03/Engineer.java
@@ -0,0 +1,26 @@
+package com.test03;
+
+public class Engineer extends Emp{
+	private String dept;
+
+	public Engineer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Engineer(String name, int salary) {
+		super(name, salary);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+", dept="+dept;
+	}
+	
+	
+}
